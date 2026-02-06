a6cef15d6d7a3583a2803f45df193a56010ec72e
kimyebin726 <126140483+kimyebin726@users.noreply.github.com>
2026-02-06 17:48:32 +0900

    Add files via upload

--- auth/firebase.js
+++ auth/firebase.js
@@ -1,17 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// 🔧 Your Firebase config
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCRCZ1aZtdGgkbSOMAWrXLViDoZuAoCfLk",
   authDomain: "project-31962698-a25c-49c1-ac5.firebaseapp.com",
   projectId: "project-31962698-a25c-49c1-ac5",
-  storageBucket: "project-31962698-a25c-49c1-ac5.firebasestorage.app",
-  messagingSenderId: "153683084088",
   appId: "1:153683084088:web:75c28d4de59f2cd03477cd"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
